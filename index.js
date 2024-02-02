14d2cae2dab2e4c9a9c3b9df53f6d5d30f15af9b
simpleworld-ideas Akbar <akbarsuhaib10@gmail.com>
2024-02-02 04:15:15 +0000

    firstcommit

---
+++ index.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <label>First Name</label>
+        <input type="text" id="firstName"/>
+    </div>
+    <div>
+        <label>Last Name</label>
+        <input type="text" id="lastName"/>
+    </div>
+    <div>
+        <label>Favorite Hobby</label>
+        <input type="radio" class="hobbies" name="hobbies" value="reading"/><label>Reading</label>
+        <input type="radio" class="hobbies" name="hobbies" value="swimming"/><label>Swimming</label>
+        <input type="radio" class="hobbies" name="hobbies" value="running"/><label>Running</label>
+    </div>
+    <div>
+        <label>Educational Level</label>
+        <input type="radio" class="level" name="level" value="primary"/><label>Primary</label>
+        <input type="radio" class="level" name="level" value="secondary"/><label>Secondary</label>
+        <input type="radio" class="level" name="level" value="tertiary"/><label>Tertiary</label>
+        <input type="radio" class="level" name="level" value="postgrad"/><label>Postgrad</label>
+    </div>
+    <div>
+        <label>Scent that you Like (more than one is possible)</label>
+        <input type="checkbox" class="scent" name="scent" value="lemongrass"/><label>Lemongrass</label>
+        <input type="checkbox" class="scent" name="scent" value="lavender"/><label>Lavender</label>
+        <input type="checkbox" class="scent" name="scent" value="durian"/><label>Durian</label>
+        <input type="checkbox" class="scent" name="scent" value="orange"/><label>Orange</label>
+    </div>
+    <div id="cuisine">
+        <!-- Create checkboxes for favourite cuisine: Chinese, Indian, Western, Japanese or Mexican
+        The user can select more than one -->
+        <label>Favorite Cuisine (can select more than one)</label>
+        <input type="checkbox" class="cuisine" name="cuisine" value="chinese"/><label>Chinese</label>
+        <input type="checkbox" class="cuisine" name="cuisine" value="indian"/><label>Indian</label>
+        <input type="checkbox" class="cuisine" name="cuisine" value="malay"/><label>Malay</label>
+        <input type="checkbox" class="cuisine" name="cuisine" value="western"/><label>Western</label>
+    </div> 
+    <div>
+        <label>Country</label>
+        <select id="country">
+            <option value="singapore">Singapore</option>
+            <option value="malaysia">Malaysia</option>
+            <option value="indonesia">Indonesia</option>
+        </select>
+    </div>
+    <button id="submitBtn">Submit</button>
+    <div id="output-container" style="display:none">
+        <h1>Your Submmission Summary</h1>
+        <div id="output"></div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
