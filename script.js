e9020e8b11c5344971bb4a6f70ae7e8c6fa9eb6c
Your Name <akbarsuhaib10@gmail.com>
2024-02-02 14:46:54 +0800

    last

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const SubmitButton= document.querySelector("#submitBtn");
+const SubmitButton = document.querySelector("#submitBtn");
 SubmitButton.addEventListener("click",function(){
     const Fname= document.querySelector("#firstName");
     const FnameValue = Fname.value;
     console.log(FnameValue);
+
+
+const outPut = document.querySelector("#output");
+outPut.innerHTML +=` 
+<ul>
+<li>Name: ${FnameValue}</li>
+</ul>
+`;
 })
